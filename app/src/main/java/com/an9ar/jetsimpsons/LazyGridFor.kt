5bf5f0f850e0f51594c4c7e6a6001fafd306ca2e
Dmitriy <63235125+an9ar@users.noreply.github.com>
2020-12-05 01:14:33 +0300

    Migration to compose version alpha08, kotlin version 1.4.20

--- app/src/main/java/com/an9ar/jetsimpsons/LazyGridFor.kt
+++ app/src/main/java/com/an9ar/jetsimpsons/LazyGridFor.kt
@@ -23,7 +23,7 @@ fun <T> LazyGridFor(
 
         Row {
             item.forEachIndexed { rowIndex, item ->
-                Box(modifier = Modifier.weight(1F).align(Alignment.Top).padding(8.dp), alignment = Alignment.Center) {
+                Box(modifier = Modifier.weight(1F).align(Alignment.Top).padding(8.dp), contentAlignment = Alignment.Center) {
                     itemContent(item, index * rows + rowIndex)
                 }
             }
